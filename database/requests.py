8d60bba2ff48a88c6ec1f39c706ff17555d81523
ECLIPSE S.Rom <pomah7878@gmail.com>
2026-08-06 19:19:55 +0300

    Пробный период по группам тарифов (шаг 2г/6): критично — добавлен импорт db_trial_groups в requests.py

--- database/requests.py
+++ database/requests.py
@@ -13,6 +13,7 @@ from database.db_servers import *
 from database.db_tariffs import *
 from database.db_stats import *
 from database.db_groups import *
+from database.db_trial_groups import *
 from database.db_settings import *
 from database.db_pages import *
 from database.db_page_routes import *
