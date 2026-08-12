454c0d9b5ffed3afae739911f983b6d56fcc390b
ECLIPSE S.Rom <pomah7878@gmail.com>
2026-08-12 09:15:38 +0300

    Импорт ручных клиентов панели (шаг 5/5): подключение router

--- bot/handlers/admin/__init__.py
+++ bot/handlers/admin/__init__.py
@@ -24,6 +24,7 @@ from bot.handlers.admin.support import router as support_router
 from bot.handlers.admin.customization_reset import router as customization_reset_router
 from bot.handlers.admin.integrations import router as integrations_router
 from bot.handlers.admin.channel_posts import router as channel_posts_router
+from bot.handlers.admin.panel_import import router as panel_import_router
 
 admin_router = Router()
 
@@ -48,4 +49,5 @@ admin_router.include_router(promotions_router)
 admin_router.include_router(customization_reset_router)
 admin_router.include_router(integrations_router)
 admin_router.include_router(channel_posts_router)
+admin_router.include_router(panel_import_router)
 
